93996e41aaa21b6fb26187c8a2cfed9c2240f064
John <jchristy40@gmail.com>
2024-05-13 16:59:31 -0500

    add marked for md files

--- app/reviews/stardew-valley/page.tsx
+++ app/reviews/stardew-valley/page.tsx
@@ -1,14 +1,16 @@
 import { readFile } from 'node:fs/promises';
+import { marked } from 'marked';
 import Heading from "@/components/Heading";
 
 export default async function StardewValleyPage() {
     const text = await readFile('./content/reviews/stardew-valley.md', 'utf8');
+    const html = marked(text);
     
     return (
         <>
         <Heading >Stardew Valley</Heading >
         <img src='/images/stardew-valley.jpg' alt='' width="640" height='360' className="rounded mb-2" />
-        <p>{text}</p>
+        <article dangerouslySetInnerHTML={{__html: html }} />
         </>
     )
 }
